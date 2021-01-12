75e356a17ebbfff28bbd8393347289c1c3310d8a
danganhvu <danganhvu1998@gmail.com>
2021-01-12 23:08:30 +0900

    balance

--- src/middleware/socket.io/gameEvent/goodWeatherEvent.js
+++ src/middleware/socket.io/gameEvent/goodWeatherEvent.js
@@ -4,9 +4,9 @@ export const event = { ...baseEvent };
 
 event.name = 'WEATHER IS GREAT';
 event.description = 'Tree grow way faster';
-event.occorPossibility = 0.005;
+event.occorPossibility = 0.05;
 event.affect = {
-    economy: +5, society: 3, environment: +20, income: +2,
+    economy: +2, society: 3, environment: +5, income: +2,
 };
 
 event.checkOccurCondition = (roomStat) => true;
