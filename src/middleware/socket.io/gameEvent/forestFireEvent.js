19c6340c41be30efba3ffe0902146d37b4f10e39
danganhvu <danganhvu1998@gmail.com>
2021-01-12 22:34:00 +0900

    balance

--- src/middleware/socket.io/gameEvent/forestFireEvent.js
+++ src/middleware/socket.io/gameEvent/forestFireEvent.js
@@ -4,9 +4,9 @@ export const event = { ...baseEvent };
 
 event.name = 'FOREST FIRE';
 event.description = 'burn, hot, no!';
-event.occorPossibility = 0.03;
+event.occorPossibility = 0.02;
 event.affect = {
-    economy: -3, society: -3, environment: -20, income: -2,
+    economy: -3, society: -3, environment: -7, income: -2,
 };
 
 event.checkOccurCondition = (roomStat) => true;
